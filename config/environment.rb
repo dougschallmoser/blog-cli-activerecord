4509490bdf6419ac8f7717e9cbf8f2c0a7a4c1c7
Doug Schallmoser <dougschallmoser@mgail.com>
2020-08-11 12:18:16 -0700

    updates loading of gems again

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,9 @@
 require 'bundler'
 Bundler.require
-require 'sinatra/activerecord/rake'
+require 'sinatra/activerecord'
 
 require_all 'app'
+
 
 connection_details = YAML::load(File.open('config/database.yml'))
 ActiveRecord::Base.establish_connection(connection_details)
